4a2531bbe2ee7877c57c096a053841c5301efd5a
bturnbull <github@brianturnbull.com>
2018-01-31 11:25:44 -0500

    Dice rolling without advantage

--- lib/dicer.ex
+++ lib/dicer.ex
@@ -1,18 +1,129 @@
 defmodule Dicer do
   @moduledoc """
-  Documentation for Dicer.
+  Evaluate and roll an RPG dice expression.
   """
 
   @doc """
-  Hello world.
+  Roll the result of an RPG dice expression.  Understands applying
+  advantage or disadvantage by appending 'a' or 'd' to a dice
+  expression.
+
+  Returns `{:ok, %{roll:, dice:, expr:}`
+  Returns `{:error, :invalid_expr}`
 
   ## Examples
 
-      iex> Dicer.hello
-      :world
+      iex> Dicer.roll("3d6")
+      %{roll: 13, dice: [[3, 4, 6]], expr: "(3 4 6)"}
+
+      iex> Dicer.roll("2d8 + 5")
+      %{roll: 12, dice: [[3, 4], 5], expr: "(3 4) + 5"}
+
+      iex> Dicer.roll("d20a + 2")
+      %{roll: 16, dice: [[11, 14], 2], expr: "(14 | 11) + 2"}
 
   """
-  def hello do
-    :world
+  def roll(expr) do
+    case :dice_lexer.string(to_charlist(expr)) do
+      {:ok, tokens, _} ->
+        {:ok, calc(tokens, %{roll: 0, dice: [], expr: ""})}
+      {:error, _, _} ->
+        {:error, :invalid_expr}
+    end
+  end
+
+  @doc """
+  Roll `count` dice of `sides` number of sides. Can also use an RPG
+  expression.
+
+  Returns `List`
+
+  ## Examples
+
+    iex> Dicer.dice(3, 6)
+    [3, 4, 1]
+
+    iex> Dicer.dice("3d6")
+    [3, 6, 2]
+
+  """
+  def dice(count, sides) do
+    Enum.to_list(1..count)
+    |> Enum.map(fn _x -> (:rand.uniform(sides)) end)
+  end
+
+  def dice(expr) do
+    [count, sides] =
+      String.split(expr, "d", parts: 2)
+      |> Enum.map(&(String.to_integer(&1)))
+    dice(count, sides)
+  end
+
+  # Accumulate over the tokens returned by the :dice_lexer, producing:
+  #
+  #   %{
+  #     roll: 14,              # final roll result
+  #     expr: "(3 5 6)",       # individual dice results (String)
+  #     dice: [3, 5, 6]        # individual dice results (List)
+  #   }
+  #
+  defp calc([head | tail], accumulator) do
+    case head do
+      {:op, _, op} ->
+        calc(tail, accumulate(%{op: op}, accumulator))
+      {:int, _, int} ->
+        calc(tail, accumulate(%{int: int}, accumulator))
+      {:dice, _, expr} ->
+        calc(tail, accumulate(%{dice: Enum.sort(dice(expr))}, accumulator))
+      {:dice_advantage, _, expr} ->
+        ## TODO
+        calc(tail, accumulator)
+      {:dice_disadvantage, _, expr} ->
+        ## TODO
+        calc(tail, accumulator)
+    end
+  end
+
+  defp calc([], accumulator) do
+    %{accumulator | expr: String.trim_leading(accumulator[:expr])}
+  end
+
+  defp accumulate(%{op: op}, acc) do
+    %{
+      roll: acc[:roll],
+      op:   op,
+      dice: acc[:dice],
+      expr: Enum.join([acc[:expr], op], " ")
+    }
+  end
+
+  defp accumulate(%{int: int}, acc) do
+    roll = case acc[:op] do
+      "-" ->
+        acc[:roll] - int
+      _ ->
+        acc[:roll] + int
+    end
+
+    %{
+      roll: roll,
+      dice: Enum.reverse([int | Enum.reverse(acc[:dice])]),
+      expr: Enum.join([acc[:expr], "#{int}"], " ")
+    }
+  end
+
+  defp accumulate(%{dice: dice}, acc) do
+    roll = case acc[:op] do
+      "-" ->
+        acc[:roll] - Enum.sum(dice)
+      _ ->
+        acc[:roll] + Enum.sum(dice)
+    end
+
+    %{
+      roll: roll,
+      dice: Enum.reverse([dice | Enum.reverse(acc[:dice])]),
+      expr: Enum.join([acc[:expr], "(#{Enum.join(dice, " ")})"], " ")
+    }
   end
 end
--- test/dicer_test.exs
+++ test/dicer_test.exs
@@ -1,8 +1,128 @@
 defmodule DicerTest do
   use ExUnit.Case
-  doctest Dicer
 
-  test "greets the world" do
-    assert Dicer.hello() == :world
+  @iterations 10000
+
+  describe "roll/1" do
+
+    test "3d6" do
+      rolls =
+        Enum.to_list(1..@iterations)
+        |> Enum.map(fn _x -> Dicer.roll("3d6") end)
+
+      assert Enum.all?(rolls, fn roll -> elem(roll, 0) == :ok end)
+      assert Enum.all?(rolls, fn roll -> elem(roll, 1).roll >= 3 end)
+      assert Enum.all?(rolls, fn roll -> elem(roll, 1).roll <= 18 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(Enum.at(elem(roll, 1).dice, 0)) >= 3 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(Enum.at(elem(roll, 1).dice, 0)) <= 18 end)
+      assert Enum.all?(rolls, fn roll -> Regex.match?(~r/^\(\d \d \d\)$/, elem(roll, 1).expr) end)
+    end
+
+    test "1d2 + 97" do
+      rolls =
+        Enum.to_list(1..@iterations)
+        |> Enum.map(fn _x -> Dicer.roll("1d2 + 97") end)
+
+      assert Enum.all?(rolls, fn roll -> elem(roll, 0) == :ok end)
+      assert Enum.all?(rolls, fn roll -> elem(roll, 1).roll >= 98 end)
+      assert Enum.all?(rolls, fn roll -> elem(roll, 1).roll <= 99 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(Enum.at(elem(roll, 1).dice, 0)) >= 1 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(Enum.at(elem(roll, 1).dice, 0)) <= 2 end)
+      assert Enum.all?(rolls, fn roll -> Enum.at(elem(roll, 1).dice, 1) == 97 end)
+      assert Enum.all?(rolls, fn roll -> Regex.match?(~r/^\(\d\) \+ 97$/, elem(roll, 1).expr) end)
+    end
+
+    test "10d4 - 5" do
+      rolls =
+        Enum.to_list(1..@iterations)
+        |> Enum.map(fn _x -> Dicer.roll("10d4 - 5") end)
+
+      assert Enum.all?(rolls, fn roll -> elem(roll, 0) == :ok end)
+      assert Enum.all?(rolls, fn roll -> elem(roll, 1).roll >= 5 end)
+      assert Enum.all?(rolls, fn roll -> elem(roll, 1).roll <= 35 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(Enum.at(elem(roll, 1).dice, 0)) >= 10 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(Enum.at(elem(roll, 1).dice, 0)) <= 40 end)
+      assert Enum.all?(rolls, fn roll -> Enum.at(elem(roll, 1).dice, 1) == 5 end)
+      assert Enum.all?(rolls, fn roll -> Regex.match?(~r/^\((\d ){9}\d\) - 5$/, elem(roll, 1).expr) end)
+    end
+
+    test "d8 + 3d6" do
+      rolls =
+        Enum.to_list(1..@iterations)
+        |> Enum.map(fn _x -> Dicer.roll("d8 + 3d6") end)
+
+      assert Enum.all?(rolls, fn roll -> elem(roll, 0) == :ok end)
+      assert Enum.all?(rolls, fn roll -> elem(roll, 1).roll >= 4 end)
+      assert Enum.all?(rolls, fn roll -> elem(roll, 1).roll <= 26 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(Enum.at(elem(roll, 1).dice, 0)) >= 1 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(Enum.at(elem(roll, 1).dice, 0)) <= 8 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(Enum.at(elem(roll, 1).dice, 1)) >= 3 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(Enum.at(elem(roll, 1).dice, 1)) <= 18 end)
+      assert Enum.all?(rolls, fn roll -> Regex.match?(~r/^\(\d\) \+ \(\d \d \d\)$/, elem(roll, 1).expr) end)
+    end
+
+    test "trash" do
+      assert {:error, _} = Dicer.roll("trash")
+    end
+  end
+
+  describe "dice/2" do
+
+    test "5d20" do
+      rolls =
+        Enum.to_list(1..@iterations)
+        |> Enum.map(fn _x -> Dicer.dice(5, 20) end)
+
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) >= 5 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) <= 100 end)
+    end
+
+    test "1d2" do
+      rolls =
+        Enum.to_list(1..@iterations)
+        |> Enum.map(fn _x -> Dicer.dice(1, 2) end)
+
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) >= 1 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) <= 2 end)
+    end
+
+    test "1d100" do
+      rolls =
+        Enum.to_list(1..@iterations)
+        |> Enum.map(fn _x -> Dicer.dice(1, 100) end)
+
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) >= 1 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) <= 100 end)
+    end
+  end
+
+  describe "dice/1" do
+
+    test "5d20" do
+      rolls =
+        Enum.to_list(1..@iterations)
+        |> Enum.map(fn _x -> Dicer.dice("5d20") end)
+
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) >= 5 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) <= 100 end)
+    end
+
+    test "1d2" do
+      rolls =
+        Enum.to_list(1..@iterations)
+        |> Enum.map(fn _x -> Dicer.dice("1d2") end)
+
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) >= 1 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) <= 2 end)
+    end
+
+    test "1d100" do
+      rolls =
+        Enum.to_list(1..@iterations)
+        |> Enum.map(fn _x -> Dicer.dice("1d100") end)
+
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) >= 1 end)
+      assert Enum.all?(rolls, fn roll -> Enum.sum(roll) <= 100 end)
+    end
   end
 end
